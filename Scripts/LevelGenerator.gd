0fa4e8f928eac75f1765cf762a3dd0801acf8b24
MyUsername <delanoleslieprof@gmail.com>
2023-12-02 22:12:23 -0800

    Add level peices

--- Scripts/LevelGenerator.gd
+++ Scripts/LevelGenerator.gd
@@ -1,16 +1,21 @@
 
+class_name LevelGenerator
+
 extends Node3D
 
-# Creates levels based off node peices
-@export var peices: Array[PackedScene] = [] # All peices that can be generated
+# Creates levels based off node pieces
+@export var start_piece: PackedScene = null # The starting piece
+@export var end_piece: PackedScene = null # The ending piece
+@export var pieces: Array[PackedScene] = [] # All pieces that can be generated
 
 func _ready():
-    # Generate the level
-    load_peices()
-    generate()
+	# Generate the level
+	generate()
 
-func load_peices():
-    
 
 func generate():
 
+	# WE Start by spawning the start piece
+	var start = start_piece.instantiate()
+	add_child(start)
+	
